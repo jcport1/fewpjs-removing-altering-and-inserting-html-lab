c62a79f7c879fbcc92aadc112a09ce9582ec23fc
jeffersoncuartas <jefferson.cuartas@gmail.com>
2021-01-28 14:50:40 -0500

    commit tree efd834324cdfc601d141c3116f7a5a01d758bf2c

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+document.querySelector("main#main").remove();
+
+//set document.createElement equal to a const
+const newHeader = document.createElement("h1");
+
+//call the methods on new header to assign values 
+newHeader.id = "victory"
+newHeader.innerHTML = "Flatiron School is the champion!";
+newHeader.className = "victory";
